0ef5d59a3ada36cf393217a9667d26580f684d35
Corbin <corbinivon@gmail.com>
2023-08-08 09:58:13 -0700

    Removed example usage

--- toast.py
+++ toast.py
@@ -4,9 +4,9 @@ def send_notification(title, message):
   notification.notify(
     title=title,
     message=message,
-    app_name="MyApp",
+    app_name="toaster",
     timeout=10  # Notification duration in seconds
   )
 
 # Example usage:
-send_notification("Hello", "This is a test notification!")
+# send_notification("Hello", "This is a test notification!")
